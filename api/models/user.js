0ad4035b1c5782b92f7f683bf07a51fdd93b0ff0
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-12-29 10:17:55 +0700

    email regex

--- api/models/user.js
+++ api/models/user.js
@@ -5,6 +5,8 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
   },
   password: {
     type: String,
