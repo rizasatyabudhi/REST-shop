e77631fde3142488ccdac75fb9958ba572f79c4d
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-12-30 19:20:46 +0700

    order controller async/await refactor

--- api/controller/ordersController.js
+++ api/controller/ordersController.js
@@ -3,91 +3,117 @@ const Order = require('../models/order');
 const Product = require('../models/product');
 
 
-exports.getOrders = (req, res) => {
-  Order
-    .find()
-    .select('product quantity _id')
-    // 1st param = field name of "product" in Order model
-    // 2nd param = what to take from Product model
-    .populate('product', 'name')
-    .exec()
-    .then((orders) => {
-      res.status(200).json({
-        count: orders.length,
-        orders: orders.map(order => ({
-          _id: order.id,
-          product: order.product,
-          quantity: order.quantity,
-          request: {
-            type: 'GET',
-            url: `http://localhost:3000/orders/${order.id}`,
-          },
-        })),
+exports.getOrders = async (req, res) => {
+  try {
+    const orders = await Order
+      .find()
+      .select('product quantity _id')
+      // 1st param = field name of "product" in Order model
+      // 2nd param = what to take from Product model
+      .populate('product', 'name')
+      .exec();
 
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-      });
+    res.status(200).json({
+      count: orders.length,
+      orders: orders.map(order => ({
+        _id: order.id,
+        product: order.product,
+        quantity: order.quantity,
+        request: {
+          type: 'GET',
+          url: `http://localhost:3000/orders/${order.id}`,
+        },
+      })),
+
     });
+  } catch (err) {
+    res.status(500).json({
+      error: err,
+    });
+  }
 };
 
-exports.getOneOrder = (req, res) => {
-  Order.findById(req.params.orderId)
+exports.getOneOrder = async (req, res) => {
+  const order = await Order.findById(req.params.orderId)
     .select('quantity productId _id')
     .populate('product', 'name')
-    .exec()
-    .then((order) => {
-      if (!order) {
-        res.status(404).json({
-          message: 'Order Not Found',
-        });
-      }
-      res.status(200).json(order);
+    .exec();
+
+  // if order not found
+  if (!order) {
+    res.status(404).json({
+      message: 'Order Not Found',
     });
+  }
+
+  // if order is found
+  res.status(200).json(order);
 };
 
-exports.addOrder = (req, res) => {
-  Product.findById(req.body.productId)
-    .then((product) => {
-      if (!product) {
-        res.status(404).json({
-          message: 'Product not found',
-        });
-      }
-      const order = new Order({
-        _id: new mongoose.Types.ObjectId(),
-        quantity: req.body.quantity,
-        product: req.body.productId,
-      });
-      return order.save();
-    })
-    .then((result) => {
-      res.status(201).json({
-        message: 'Order Stored',
-        createdOrder: {
-          _id: result.id,
-          product: result.product,
-          quantity: result.quantity,
-        },
-      });
+exports.addOrder = async (req, res) => {
+  const product = await Product.findById(req.body.productId);
+  if (!product) {
+    res.status(404).json({
+      message: 'Product not found',
     });
+  }
+
+  const order = await new Order({
+    _id: new mongoose.Types.ObjectId(),
+    quantity: req.body.quantity,
+    product: req.body.productId,
+  }).save();
+
+  res.status(201).json({
+    message: 'Order Stored',
+    createdOrder: {
+      _id: order.id,
+      product: order.product,
+      quantity: order.quantity,
+    },
+  });
 };
 
+// exports.addOrder = (req, res) => {
+//   Product.findById(req.body.productId)
+//     .then((product) => {
+//       if (!product) {
+//         res.status(404).json({
+//           message: 'Product not found',
+//         });
+//       }
+//       const order = new Order({
+//         _id: new mongoose.Types.ObjectId(),
+//         quantity: req.body.quantity,
+//         product: req.body.productId,
+//       });
+//       return order.save();
+//     })
+//     .then((result) => {
+//       res.status(201).json({
+//         message: 'Order Stored',
+//         createdOrder: {
+//           _id: result.id,
+//           product: result.product,
+//           quantity: result.quantity,
+//         },
+//       });
+//     });
+// };
 
-exports.removeOrder = (req, res) => {
+
+exports.removeOrder = async (req, res) => {
   const id = req.params.orderId;
-  Order.findByIdAndRemove(id)
-    .exec()
-    .then((order) => {
-      if (!order) {
-        res.status(404).json({
-          message: 'Order Not Found',
-        });
-      }
-      res.status(200).json({
-        message: 'Order was removed',
-      });
+  const order = await Order.findByIdAndRemove(id).exec();
+  // if order is not found
+  if (!order) {
+    res.status(404).json({
+      message: 'Order Not Found',
     });
+  }
+
+  // if order is found
+  res.status(200).json({
+    message: 'Order was removed',
+  });
 };
